7fb5a1c54aa61bf82ebd14df2a6cdcd92f9fb95e
erizc-dl <ericz.dlong@gmail.com>
2025-12-02 11:08:03 -0500

    added digits and index to simplify command selection process

--- commands.py
+++ commands.py
@@ -11,15 +11,31 @@ commands = [
     "git pull", 
     "git push"]
 
-list_of_commands = "Top Git Commands: \n1. git init \n2. git clone \n3. git add \n4. git commit \n5. git status \n6. git branch \n7. git merge \n8. git pull \n9. git push \n"
+list_of_commands = (
+    "Top Git Commands:\n"
+    "1. git init\n"
+    "2. git clone\n"
+    "3. git add\n"
+    "4. git commit\n"
+    "5. git status\n"
+    "6. git branch\n"
+    "7. git merge\n"
+    "8. git pull\n"
+    "9. git push\n"
+)
+
 print(list_of_commands)
 
+# Asks the user for a command
 command = input("Enter your command (1-9): ")
 
-if command in commands:
-    print("Valid command")
-else:
-    while command not in commands:
-        print("Invalid command")
-        input("Enter your command: ")
-    print("Valid command")
+# Validate user's input
+while not command.isdigit() or not (1 <= int(command) <= 9):
+    command = input("Invalid input. Enter a number between 1 and 9: ")
+
+# Transform input to index
+index = int(command) - 1
+
+# Return final command
+selected_command = commands[index]
+print(f"You've selected {selected_command}.")
